caadc917c090d4dbc1c62ead684e3f82dca8c3ef
jamiegall <141264811+jamiegall@users.noreply.github.com>
2023-08-23 12:51:34 -0400

    New Files for Master Data Set (Correct)

--- Master_set.R
+++ Master_set.R
@@ -19,15 +19,29 @@ library(data.table)
 mods_all <- read.csv("mods_all.csv")
 mods_all_2 <- read.csv("mods_all_2.csv")
 
-mods_all_public = bind_rows(
-  mods_all %>% anti_join(mods_all_2, by="Title"),
-  mods_all_2)
+#subset mod data
+values_to_exclude <- c("Private Laws", "Concurrent Resolutions", "Reorganization Plan", "Back Matter", "Constitutional Amendment", "Unknown", "Sub-Volume")
+
+# Filter out rows where partName is in the list of values to exclude
+mods_all_a <- mods_all[!mods_all$partName %in% values_to_exclude, ]
 
 #subset mod data
 values_to_exclude <- c("Private Laws", "Concurrent Resolutions", "Reorganization Plan", "Back Matter", "Constitutional Amendment", "Unknown", "Sub-Volume")
 
 # Filter out rows where partName is in the list of values to exclude
-mods_all_public <- mods_all[!mods_all$partName %in% values_to_exclude, ]
+mods_all_2_a <- mods_all_2[!mods_all_2$partName %in% values_to_exclude, ]
+
+# Find the rows in mods_all_2 that have differing "Date" values
+differences <- mods_all_2_a %>%
+  anti_join(mods_all_a, by = "Title") %>%
+  select(Title, Date)  # Select only "Title" and "Date" columns
+
+# Combine the differing "Date" values with mods_all_2
+mods_all_public <- mods_all_2_a %>%
+  left_join(differences, by = "Title", suffix = c("_2", "_1")) %>%
+  mutate(Date = ifelse(!is.na(Date_1), Date_1, Date_2)) %>%
+  select(-Date_1, -Date_2)  # Remove the temporary "Date_1" and "Date_2" columns
+
 
 #clean all mods
 mods_all_public$PublicLaw <- gsub("Public Law ", "", mods_all_public$PublicLaw)
@@ -66,17 +80,22 @@ Mods <- read.csv("MODS_fixes.csv")
 hein_data_raw$Source <- "Hein Online"
 Mods$Source <- "GPO"
 
-hein_data_raw_2 <- hein_data_raw %>%
-  filter(congress_number < 107)
+hein_data_raw$title[is.na(hein_data_raw$title) | hein_data_raw$title == ""] <- NA
 
+hein_data_raw_1 <- hein_data_raw %>%
+  filter(!is.na(title))
 
+hein_data_raw_2 <- hein_data_raw_1 %>%
+  filter(sal_volume <=112 & sal_volume > 64)
+Mods_2 <- Mods%>%
+  filter(sal_volume <=112)
+ 
 filtered_late_hein <- hein_data_raw_2 %>%
-  filter(congress_number >= 86)
-filtered_late_mods <- Mods %>%
-  filter(pl_no >= 86-0)
+  filter(sal_volume >= 73)
+filtered_late_mods <- Mods_2 %>%
+  filter(sal_volume >=73)
 
-
-master1 <- merge(filtered_late_hein, filtered_late_mods, by = "pl_no", all = TRUE)
+master1 <- merge(filtered_late_hein, filtered_late_mods, by = "pl_no", all.y = TRUE)
 
 master1$sal_page_start.x <- as.integer(master1$sal_page_start.x)
 
@@ -88,11 +107,12 @@ master1 <- master1 %>%
   select(pl_no, sal_volume, sal_page_start, date, chapter, congress_number, session_number, Source, action, Title, Date, StatuteCitation, BillCitation, URL)
 
 filtered_mid_hein <- hein_data_raw_2 %>%
-  filter(congress_number < 86)
+  filter(sal_volume < 73)
 filtered_mid_mods <- Mods %>%
-  filter(pl_no < 86-0)
+  filter(sal_volume <73)
 
-master2 <- merge(filtered_mid_hein, filtered_mid_mods, by = c("sal_volume", "sal_page_start"), all = TRUE)
+master2 <- merge(filtered_mid_hein, filtered_mid_mods, by = c("sal_volume", "sal_page_start"), all.y = TRUE)
+
 
 master2 <- master2 %>%
   mutate(pl_no = coalesce(pl_no.x, pl_no.y)) %>%
@@ -101,7 +121,7 @@ master2 <- master2 %>%
   select(pl_no, sal_volume, sal_page_start, date, chapter, congress_number, session_number, Source, action, Title, StatuteCitation, Date, BillCitation, URL)
 
   
-master3 <- merge(master2, filtered_mid_mods, by = "pl_no", all = TRUE)
+master3 <- merge(master2, filtered_mid_mods, by = "pl_no", all.y = TRUE)
 
 master3$sal_page_start.x <- as.integer(master3$sal_page_start.x)
 
@@ -127,11 +147,56 @@ master4 <- master4 %>% rename(secondary_date = date)
 master4 <- master4 %>% rename(date_of_passage = Date)
 master4 <- master4 %>% rename(dates_conflict = DatesEqual)
 
-master4 <- master4 %>%
-  arrange(sal_volume, sal_page_start) %>%
-  mutate(RowNumber = row_number())
 
 master4 <- master4 %>%
-  select(RowNumber, action, Title, sal_volume, sal_page_start, StatuteCitation, BillCitation, congress_number, chapter, session_number, pl_no, BillCitation, date_of_passage, secondary_date, dates_conflict, Source, URL ) 
+  select(action, Title, sal_volume, sal_page_start, StatuteCitation, BillCitation, congress_number, chapter, session_number, pl_no, date_of_passage, secondary_date, dates_conflict, Source, URL )
 
-write.csv(master4, file = "master_set_new.csv", row.names=FALSE)
+
+
+#add on hein pre 1951
+hein_data_raw_old <- hein_data_raw_1 %>%
+  filter(sal_volume <= 64)
+
+hein_data_raw_old$URL <- NA
+hein_data_raw_old$StatuteCitation <- NA
+hein_data_raw_old$BillCitation <- NA
+hein_data_raw_old$date_of_passage <- NA
+hein_data_raw_old$dates_conflict <- NA
+hein_data_raw_old <- hein_data_raw_old %>% rename(secondary_date = date)
+hein_data_raw_old <- hein_data_raw_old %>% rename(Title = title)
+
+hein_data_raw_old <- hein_data_raw_old %>%
+  select(action, Title, sal_volume, sal_page_start, StatuteCitation, BillCitation, congress_number, chapter, session_number, pl_no, date_of_passage, secondary_date, dates_conflict, Source, URL )
+
+hein_data_raw_old$sal_page_start <- as.integer(hein_data_raw_old$sal_page_start)
+
+master5 <- bind_rows(master4, hein_data_raw_old)
+
+
+
+#add on mods post volume 112
+Mods_new <- Mods%>%
+  filter(sal_volume >=112)
+
+Mods_new$congress_number <- as.integer(gsub("^(\\d+)-.*", "\\1", Mods_new$pl_no))
+Mods_new <- Mods_new %>% rename(date_of_passage = Date)
+Mods_new$action <- NA
+Mods_new$chapter <- NA
+Mods_new$session_number <- NA
+Mods_new$secondary_date <- NA
+Mods_new$dates_conflict <- NA
+
+Mods_new <- Mods_new %>%
+  select(action, Title, sal_volume, sal_page_start, StatuteCitation, BillCitation, congress_number, chapter, session_number, pl_no, date_of_passage, secondary_date, dates_conflict, Source, URL )
+
+master6 <- bind_rows(master5, Mods_new)
+
+master6 <- master6 %>%
+  arrange(sal_volume, date_of_passage, secondary_date) %>%
+  mutate(row_number = row_number())
+
+master6 <- master6 %>%
+  select(row_number, action, Title, sal_volume, sal_page_start, StatuteCitation, BillCitation, congress_number, chapter, session_number, pl_no, date_of_passage, secondary_date, dates_conflict, Source, URL )
+
+
+write.csv(master6, file = "master_set_new.csv", row.names=FALSE)
